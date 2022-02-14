da9a56e49861d31961904a9cbb0c03b60650be94
Cédric Rollo <cedric.rollo@iut-tlse3.fr>
2022-02-14 15:15:14 +0100

    💄 🚸 infos montants

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -1,13 +1,25 @@
 <template>
   <!-- infos / tarif -->
   <div class="row no-bot">
-    <div class="row no-bot">
-      <div class="col s12">
-        Total:
-        <strong>{{ encaisse }}</strong> encaiss&eacute;(s) /
-        <strong>{{ total }}</strong> inscrit(s)
-      </div>
-      <div class="col s12">
+    <div class="col s12">
+      Total:
+      <strong>{{ encaisse }}</strong> encaiss&eacute;(s) /
+      <strong>{{ total }}</strong> inscrit(s)
+      <button
+        class="btn-flat btn-small waves-effect waves-blue-grey blue-grey lighten-4"
+        @click="showMontants = !showMontants"
+      >
+        montants
+        <i class="material-icons right" v-if="showMontants === false">
+          <font-awesome-icon icon="angle-double-down" size="sm" />
+        </i>
+        <i class="material-icons right" v-if="showMontants === true">
+          <font-awesome-icon icon="angle-double-up" size="sm" />
+        </i>
+      </button>
+    </div>
+
+    <div class="col s12" v-show="showMontants">
         <div class="input-field inline">
           Tarif (€/personne) :&nbsp;
           <input
@@ -19,66 +31,67 @@
             @change="$emit('tarif', tarif)"
           >
         </div>
-      </div>
+
+        <div>
+          <div class="col s2">%1er</div>
+          <div class="col s2">%2nd</div>
+          <div class="col s2">%3eme</div>
+          <div class="col s2">%Club</div>
+          <div class="col s2">%Bar</div>
+        </div>
+        <br>
+
+        <!-- % des gains -->
+        <div>
+          <div class="input-field col s2">
+            <input
+              v-model="r1"
+              id="ratio1st"
+              type="number"
+              class="validate"
+              @change="$emit('1er', r1)"
+            >
+          </div>
+          <div class="input-field col s2">
+            <input
+              id="ratio2nd"
+              v-model="r2"
+              type="number"
+              class="validate"
+              @change="$emit('2nd', r2)"
+            >
+          </div>
+          <div class="input-field col s2">
+            <input
+              id="ratio3rd"
+              v-model="r3"
+              type="number"
+              class="validate"
+              @change="$emit('3eme', r3)"
+            >
+          </div>
+          <div class="input-field col s2">
+            <input
+              id="ratioClub"
+              v-model="rClub"
+              type="number"
+              class="validate"
+              @change="$emit('club', rClub)"
+            >
+          </div>
+          <div class="input-field col s2">
+            <input
+              id="ratioBar"
+              v-model="rBar"
+              type="number"
+              class="validate"
+              @change="$emit('bar', rBar)"
+            >
+          </div>
+        </div>
+        <!-- end .row % des gains -->
     </div>
 
-    <div class="row no-bot">
-      <div class="col s2">%1er</div>
-      <div class="col s2">%2nd</div>
-      <div class="col s2">%3eme</div>
-      <div class="col s2">%Club</div>
-      <div class="col s2">%Bar</div>
-    </div>
-
-    <!-- % des gains -->
-    <div class="row no-bot">
-      <div class="input-field col s2">
-        <input
-          v-model="r1"
-          id="ratio1st"
-          type="number"
-          class="validate"
-          @change="$emit('1er', r1)"
-        >
-      </div>
-      <div class="input-field col s2">
-        <input
-          id="ratio2nd"
-          v-model="r2"
-          type="number"
-          class="validate"
-          @change="$emit('2nd', r2)"
-        >
-      </div>
-      <div class="input-field col s2">
-        <input
-          id="ratio3rd"
-          v-model="r3"
-          type="number"
-          class="validate"
-          @change="$emit('3eme', r3)"
-        >
-      </div>
-      <div class="input-field col s2">
-        <input
-          id="ratioClub"
-          v-model="rClub"
-          type="number"
-          class="validate"
-          @change="$emit('club', rClub)"
-        >
-      </div>
-      <div class="input-field col s2">
-        <input
-          id="ratioBar"
-          v-model="rBar"
-          type="number"
-          class="validate"
-          @change="$emit('bar', rBar)"
-        >
-      </div>
-    </div>
-    <!-- end .row % des gains -->
   </div>
   <!-- end .row infos / tarif -->
 </template>
@@ -102,6 +115,7 @@ export default {
     r3: 0,
     rClub: 0,
     rBar: 0,
+    showMontants: false,
   }),
   computed: {
     // nombre de joueurs inscrits
--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEye, faEyeSlash, faPiggyBank, faTrash, faAngleUp, faCopy, faUser, faSort } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash, faPiggyBank, faTrash, faAngleUp, faCopy, faUser, faSort, faWrench, faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from 'vue';
 import VueClipboard from 'vue-clipboard2';
 import App from './App.vue';
 
-library.add(faEye, faEyeSlash, faPiggyBank, faTrash, faAngleUp, faCopy, faUser, faSort);
+library.add(
+  faEye,
+  faEyeSlash,
+  faPiggyBank,
+  faTrash,
+  faAngleUp,
+  faCopy,
+  faUser,
+  faSort,
+  faWrench,
+  faAngleDoubleDown,
+  faAngleDoubleUp,
+);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
